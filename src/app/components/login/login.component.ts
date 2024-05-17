6529babab884a45dfe68c1474b68da726cd597fd
Rahul <mrahulmaity623@gmail.com>
2024-05-17 09:24:45 +0530

    Formatted css and Signup page implemented

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  type: string = "password"; // to show or hide password
+  // showPassword: boolean = false;
+  eyeIcon: string = "fa-eye-slash";
+  isText: boolean = false;
+  hideShowPass() {
+   
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText? this.type="text": this.type="password";
+  }
+
+}
