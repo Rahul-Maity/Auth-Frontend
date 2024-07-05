f444d6c831f2dc5b118de9187e36cba0c86b899e
Rahul <mrahulmaity623@gmail.com>
2024-07-05 17:17:29 +0530

    validated for login-signup

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
+  
 
-  loginForm!: FormGroup ;
+  loginForm!: FormGroup;
 
 
-  constructor(public fb:FormBuilder){}
+  constructor(public fb: FormBuilder) { }
   ngOnInit(): void {
     // throw new Error('Method not implemented.');
     this.loginForm = this.fb.group({
@@ -26,10 +27,42 @@ export class LoginComponent implements OnInit{
   eyeIcon: string = "fa-eye-slash";
   isText: boolean = false;
   hideShowPass() {
-   
+
     this.isText = !this.isText;
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
-    this.isText? this.type="text": this.type="password";
+    this.isText ? this.type = "text" : this.type = "password";
   }
+
+  onSubmit() {
+    // throw new Error('Method not implemented.');
+    if (this.loginForm.valid)
+    {
+      //send to db
+      console.log(this.loginForm.value);
+    }
+    else {
+      //throw error using toaster
+      console.log('this form is  invalid');
+      this.validateAllFormFields(this.loginForm);
+
+
+    }
+  }
+
+  private validateAllFormFields(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if (control instanceof FormControl) {
+        control?.markAsDirty({ onlySelf: true });
+      }
+      else if (control instanceof FormGroup)
+      {
+        this.validateAllFormFields(control);
+        }
+
+    });
+
+  }
+
 
 }
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+
+  signupForm!: FormGroup;
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+    this.signupForm = this.fb.group({
+      firstName:['',Validators.required],
+      lastName: ['', Validators.required],
+      userName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+  }
 
   type: string = "password"; // to show or hide password
   // showPassword: boolean = false;
   eyeIcon: string = "fa-eye-slash";
   isText: boolean = false;
+
+
+constructor(private fb:FormBuilder){}
   hideShowPass() {
    
     this.isText = !this.isText;
